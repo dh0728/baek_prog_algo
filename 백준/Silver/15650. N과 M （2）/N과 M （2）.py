8006eb9a6b7bf240e1f33a2344086efff5df89a4
dh0728 <155514337+dh0728@users.noreply.github.com>
2024-09-10 22:43:52 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -3,18 +3,12 @@ def find_sequence(cnt,seq):
         print(*seq)
         return
     for n in range(1,N+1):
-        if select[n]:
+        if n <= seq[-1]: # 전 숫자보다 작으면 pass
             continue
-        if n < seq[-1]:
-            continue
-        select[n]=1
         find_sequence(cnt+1,seq+[n])
-        select[n]=0
 
 # 1부터 N까지 중복없이 M개를 고른 수열
 N,M=map(int,input().split())
 select=[0]*(N+1)
-for n in range(1,N+1):
-    select[n] = 1
+for n in range(1,N+1): # 수열을 첫 숫자를 넣어 주면서 시작
     find_sequence(1,[n]) # 선택한 숫자 개수, 수열리스트
-    select[n] = 0
