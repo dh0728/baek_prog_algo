879f304ff21395b54d162b9e283f3276b9db5c9f
dh0728 <155514337+dh0728@users.noreply.github.com>
2024-09-23 23:23:32 +0900

    [Silver III] Title: 거북이, Time: 256 ms, Memory: 113764 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -5,27 +5,24 @@ def find_x_y(control):
     dxy=[[0,1],[1,0],[0,-1],[-1,0]]
     x,y=0,0
     angle=0
-    x_set=set()
-    y_set=set()
-    x_set.add(0) # 초기 좌표 넣기
-    y_set.add(0)
+    x_set=set([0])
+    y_set=set([0])
     for c in control:
         if c=='L':
-            angle=(angle-1)%4
-        elif c=='R':
             angle=(angle+1)%4
+        elif c=='R':
+            angle=(angle-1)%4
         elif c=='F': # 앞으로 한칸
-            x += dxy[angle][0]
-            y += dxy[angle][1]
+            x = x + dxy[angle][0]
+            y = y + dxy[angle][1]
             x_set.add(x)
             y_set.add(y)
         else: # 뒤로 한칸
-            x -= dxy[angle][0]
-            y -= dxy[angle][1]
+            x = x + dxy[angle-2][0]
+            y = y + dxy[angle-2][1]
             x_set.add(x)
             y_set.add(y)
     return (max(x_set)-min(x_set)) * (max(y_set)-min(y_set))
-
 T=int(input())
 for tc in range(1,T+1):
     control_list=input().strip()
