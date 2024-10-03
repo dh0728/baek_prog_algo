f2b0b05818a46c5bc87d234a15792be6015f26be
dh0728 <155514337+dh0728@users.noreply.github.com>
2024-10-03 16:11:56 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1240 ms, Memory: 291236 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,52 +1,47 @@
 import sys
-# sys.stdin = open('input.txt')
+#sys.stdin=open('input.txt')
 input = sys.stdin.readline
 from collections import deque
 
-def bfs(s, e):
+def bfs(s,e):
     global min_cnt
-    dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]  # 2차원 배열을 벽을 부순 상태와 아닌 상태로 구분
+    dxy= [[0,1],[1,0],[0,-1],[-1,0]]
+    # visited=[[0]*M for _ in range(N)]
+    visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]  # 3차원으로 벽을 부순상태와 안부순 상태 구분
 
-    q = deque()
-    q.append([s, e, 1, 1])  # x, y, 칸 수, 벽을 부술 수 있는 횟수
-    visited[s][e][1] = 1  # 시작 위치에서 벽을 부수지 않은 상태로 방문 처리
+    q=deque()
+    q.append([s,e,1,1]) # i, j, 칸수, 벽을 부술수 있는 횟수
+    visited[s][e][1]=1
 
     while q:
-        x, y, cnt, block = q.popleft()
-
-        # 목표 지점 도착하면 최소 경로 갱신
-        if x == N - 1 and y == M - 1:
-            min_cnt = min(min_cnt, cnt)
-            continue
-
+        x, y ,cnt, block= q.popleft()
+        if x==N-1 and y==M-1: # 목표지점 도착하면
+            if cnt < min_cnt: # 최소경로 더작은걸로 바꾸기
+                min_cnt =cnt
         for dx, dy in dxy:
-            ni = x + dx
-            nj = y + dy
-
-            if ni < 0 or ni >= N or nj < 0 or nj >= M:  # 경로가 범위를 벗어나면 패스
+            ni = x+dx
+            nj = y+dy
+            if ni<0 or ni>=N or nj <0 or nj >=M: #경로 벗어나기
                 continue
-
-            # 벽을 부수지 않고 이동할 수 있는 경우
-            if maps[ni][nj] == 0 and visited[ni][nj][block] == 0:
-                visited[ni][nj][block] = 1
-                q.append([ni, nj, cnt + 1, block])
-
-            # 벽을 부수고 이동해야 하는 경우
-            elif maps[ni][nj] == 1 and block == 1 and visited[ni][nj][0] == 0:
-                visited[ni][nj][0] = 1
-                q.append([ni, nj, cnt + 1, 0])
+            # 벽이 아닌 경우
+            if maps[ni][nj]==0:
+                if visited[ni][nj][block]==0:
+                    visited[ni][nj][block]=1
+                    q.append([ni,nj,cnt+1,block])
+            # 벽을 만난 경우
+            else:
+                if block and visited[ni][nj][0]==0: # 부술 횟수 남아있으면
+                    visited[ni][nj][0]=1
+                    q.append([ni,nj,cnt+1,0])
 
     return
 
-N, M = map(int, input().split())
-maps = [list(map(int, input().strip())) for _ in range(N)]
-min_cnt = N * M + 1  # 최단 경로를 초기화할 때 충분히 큰 값으로 설정
 
-bfs(0, 0)
-
-# 결과 출력
-if min_cnt == N * M + 1:
+N , M = map(int,input().split())
+maps= [list(map(int,input().strip())) for _ in range(N)]
+min_cnt=N*M+1
+bfs(0,0)
+if min_cnt == N*M+1:
     print(-1)
 else:
     print(min_cnt)
