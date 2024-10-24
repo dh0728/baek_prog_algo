404cdab678953438e13298bd1da7157a28051616
dh0728 <155514337+dh0728@users.noreply.github.com>
2024-10-24 11:11:20 +0900

    [Silver II] Title: 점프 점프, Time: 104 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -2,32 +2,18 @@ import sys
 #sys.stdin=open('input.txt')
 input = sys.stdin.readline
 
-from collections import deque
-
-
-def bfs():
-    q=deque()
-    visited = [0] * N
-    q.append([0,0]) # idx, cnt
-    visited[0]=1
-
-    while q:
-        idx, cnt =q.popleft() # 점프력
-
-        for j in range(arr[idx],0,-1): # 점프해서 갈 수 있는 칸
-            if idx+j >= N - 1:  # 오른쪽 끝에 도달 하면
-                return cnt+1  # 점프 횟수 리턴
-            if visited[idx+j]:
-                continue
-            visited[idx+j]=1
-            q.append([idx+j,cnt+1])   # 짧은 순부터 차례로 넣기
-    return -1
-
 N= int(input())
 arr=list(map(int,input().split()))
 
-if N==1:
-    print(0)
+dp = [N]*N
+dp[0]=0
+
+for i in range(N):
+    for j in range(arr[i]+1): # j = 점프로 갈 수 있는 칸 수
+        if i+j < N:
+            dp[i+j]=min(dp[i+j],dp[i]+1)
+
+if dp[N-1] < N:
+    print(dp[N-1])
 else:
-    result=bfs()
-    print(result)
+    print(-1)
