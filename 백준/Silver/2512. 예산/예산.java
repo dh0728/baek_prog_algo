66ae48c4165dcf20379e5766a2040910c7b17657
dh0728 <155514337+dh0728@users.noreply.github.com>
2025-02-25 16:32:09 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -6,52 +6,54 @@ import java.util.Collections;
 public class Main {
 
     static int N;
-    static int total =0;
-    static Integer[] arr;
+    static int[] arr;
     static int account;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new Integer[N];
+        arr = new int[N];
 
         String[] str = br.readLine().split(" ");
 
         for (int i =0 ; i < N ; i++){
             int num = Integer.parseInt(str[i]);
             arr[i] = num;
-            total += num;
         }
 
-        Arrays.sort(arr , Collections.reverseOrder()); //정렬하기
+        Arrays.sort(arr); //정렬하기
 
         account = Integer.parseInt(br.readLine());
 
-        if (account >= total){ //모든 요청이 배정될 수 있는 경우
-            System.out.println(arr[0]);
-        }else{
-            int top = arr[0]-1;
-            while (true){
-                total =0;
-                for (int i=0 ; i < N ; i++){
-                    if (arr[i] >= top){ //상한선 보다 크면
-                        total += top;
-                    } else{
-                        total += arr[i];
-                    }
 
-                }
-                if (total > account){ //예산을 초과하면 다시 시작
-                    top -=1;
-                }
-                else {
-                    break;
-                }
+        // 이진 탐색을 이용한 상한액 결정
+        int result = findUpperLimit();
+        System.out.println(result);
+
+
+    }
+    public static int findUpperLimit(){
+        int left = 0;
+        int right = arr[N-1];
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = ( left +right ) / 2;
+            int sum = 0;
+
+            for (int i = 0 ; i < N ; i++){
+                sum += Math.min(arr[i], mid);
             }
-            System.out.println(top);
-        }
 
+            if (sum <= account){ //상한선 올리기
+                ans = mid;
+                left = mid + 1;
+            } else { // 상한선 내리기
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 
 }
