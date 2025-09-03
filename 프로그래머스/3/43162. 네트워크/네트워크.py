fa731d05e3cefa12d387e182ce44c647166ad0b3
dh0728 <155514337+dh0728@users.noreply.github.com>
2025-09-04 02:15:41 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def dfs(n, nodes):
+def semi_dfs(n, nodes):
     
     cnt = 0;
     visited = [0] * (n + 1) # 방문 체크
@@ -18,17 +18,48 @@ def dfs(n, nodes):
             current_node = stack.pop()
             
             for j in range(n):
-                if nodes[current_node][j] == 0 or i == j: # 연결 안된건 패스
+                if nodes[current_node][j] == 0 or current_node == j: # 연결 안된건 패스
                     continue
                     
                 if visited[j] == 1:  # 방문한적이 있으면
                     continue
                 stack.append(j)
-                # current_node = j
                 visited[j] = 1
     
     return cnt
+       
+    
+    
+def dfs(n, nodes):
+    
+    cnt = 0;
+    visited = [0] * (n + 1) # 방문 체크
+    
+    stack = []
+    for i in range(n):
+        if visited[i] : # 방문했으면
+            continue
+            
+        current_node = i
+        cnt += 1
+        visited[i] = 1 # 방문표시하기
+        stack.append(i)
+        while stack:
+            current_node = stack[-1]
+            
+            for j in range(n):
+                if nodes[current_node][j] == 0 or current_node == j: # 연결 안된건 패스
+                    continue
                     
+                if visited[j] == 1:  # 방문한적이 있으면
+                    continue
+                stack.append(j)
+                visited[j] = 1
+                break
+            else:
+                stack.pop()
+    
+    return cnt
     
 
 def solution(n, computers):
