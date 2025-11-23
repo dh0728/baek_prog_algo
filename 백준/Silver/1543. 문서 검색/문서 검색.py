eabbbf381b4eaf84f3d4573f1b00ad42582e42c2
dh0728 <155514337+dh0728@users.noreply.github.com>
2025-11-23 23:44:28 +0900

    [Silver V] Title: 문서 검색, Time: 84 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -0,0 +1,15 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+S = input().strip()
+x = input().strip()
+i = 0
+cnt = 0
+while i < len(S):
+    if S[i:i+len(x)] == x:
+        i += len(x)
+        cnt += 1
+    else :
+        i += 1
+print(cnt)
