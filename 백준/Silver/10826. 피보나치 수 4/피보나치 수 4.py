42ce80a9693e65fb379a74149a350d9fe4c7ce0b
dh0728 <155514337+dh0728@users.noreply.github.com>
2026-03-09 14:50:50 +0900

    [Silver V] Title: 피보나치 수 4, Time: 108 ms, Memory: 115328 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,24 @@
+import sys
+#sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+if N < 3:
+    if N == 0:
+        print(0)
+    else:
+        print(1)
+else:
+    dp = [0] * (N + 1)
+    
+    dp[1] = 1
+    dp[2] = 1
+     
+    for i in range(3, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    print(dp[N])
+ 
+
+
